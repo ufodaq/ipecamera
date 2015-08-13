ad52865aefeef7c87506a45bf2515413c8092a09
Suren A. Chilingaryan <csa@suren.me>
2015-08-14 01:35:09 +0200

    Improve autotrigger test

--- tests/autotrigger.sh
+++ tests/autotrigger.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+#Example options:
+# ./autotrigger.sh --threads 4 							- use multiple threads for preprocessing
+# ./autotrigger.sh --data raw --format raw					- test raw_data_callback functionality
+# PCILIB_DEBUG_MISSING_EVENTS=1 IPECAMERA_DEBUG_HARDWARE=1 ./autotrigger.sh	- debug missing frames 
+
 function pci {
     APP_PATH=`dirname $0`/..
     if [ -d $APP_PATH/../pcitool ]; then
@@ -14,14 +19,14 @@ function pci {
 }
 
 echo "Starting the grabber"
-pci -g -o /dev/null --run-time 1002000000 --verbose 10 &
+pci -g -o /dev/null --run-time 10002000000 --verbose 10 $@ &
 pid=$!
 
-trap "{ /usr/bin/kill -s INT $!; }" SIGINT
+trap "{ kill -s INT $!; }" SIGINT
 
 sleep 0.1
 pci -w 9040 80004a01
-sleep 1000
+sleep 10000
 pci -w 9040 80000201
 
 echo "Waiting grabber to finish"
